bfc144dd0370df9df2f73a9ae036a3c76480f13f
IlhamRobbS <IlhamRobbS@LAPTOP-AUHI2QMK>
2021-11-09 22:44:57 +0700

    Add project files.

---
+++ RentalKendaraan_135/Models/Jaminan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace RentalKendaraan_135.Models
+{
+    public partial class Jaminan
+    {
+        public int IdJaminan { get; set; }
+        public string NamaJaminan { get; set; }
+    }
+}
